b58b57b593250c8bac4795a59c26cdcc8ac0e529
mgkEdits <elijahnyasiando.mn@gmail.com>
2024-02-21 07:41:32 +0300

    sign up/update password

--- src/APP/App.jsx
+++ src/APP/App.jsx
@@ -2,7 +2,8 @@ import Component from './components/Navbar'
 import NavComponent from './modules/Navbar'
 import Sidenav from './modules/Sidebar'
 import SignUpForm from './modules/authentication/Signup'
-
+import LoginForm from './modules/authentication/Login'
+import UpdatePwd from './modules/authentication/updatePwd'
 import './App.css'
 
 
@@ -15,7 +16,9 @@ function App() {
       {/* <NavComponent/> */}
       {/* <Navbar2 />
       <Sidenav /> */}
-      <SignUpForm />
+     {/*  <SignUpForm />*/} 
+      {/*  <LoginForm />*/} 
+       <UpdatePwd />
       
     </div>
   )
---
+++ src/APP/modules/authentication/authentication.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+export default function authentication() {
+  return (
+    <div className="container mx-auto px-4 py-44">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
+            <div className="col-span-1 bg-green-500 flex justify-center items-center">
+              {/* Add your image here */}
+              <img src="" alt="calender" className="w-full h-auto rounded-lg object-cover" />
+            </div>
+            <div className="col-span-1 px-8 py-4 bg-gray-100 rounded-lg shadow-md">
+              <div className="text-center mb-8">
+                <h1 className="text-4xl font-bold text-yellow-500">Sign Up</h1>
+                <div className="border-b border-black w-full mx-auto mt-4"></div>
+              </div>
+              <form action="#" className="space-y-8">
+                <div className="form-control">
+                  <label for="email" className="block text-sm font-medium text-gray-700">
+                    Enter your Email
+                  </label>
+                  <div className="relative">
+                    <input
+                      type="email"
+                      id="email"
+                      className="w-full rounded-md border border-gray-300 py-2 px-4 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                      placeholder="Email Address"
+                    />
+                  </div>
+                </div>
+                <div className="form-control">
+                  <label for="password" className="block text-sm font-medium text-gray-700">
+                    Enter your password
+                  </label>
+                  <div className="relative">
+                    <input
+                      type="password"
+                      id="password"
+                      className="w-full rounded-md border border-gray-300 py-2 px-4 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+                <div className="form-control">
+                  <label for="confirmPassword" className="block text-sm font-medium text-gray-700">
+                    Confirm password
+                  </label>
+                  <div className="relative">
+                    <input
+                      type="password"
+                      id="confirmPassword"
+                      className="w-full rounded-md border border-gray-300 py-2 px-4 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="btn bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  w-full rounded-md py-2 px-4 text-center text-base font-medium shadow-sm hover:from-pink-500 hover:to-yellow-500 active:from-yellow-400 active:to-pink-400  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Sign up
+                </button>
+                <div className="flex items-center justify-between">
+                  <hr className="w-full bg-gray-200" />
+                  <p className="text-sm text-gray-400">or</p>
+                  <hr className="w-full bg-gray-200" />
+                </div>
+                <a href="#" className="text-sm text-center text-blue-600 hover:underline">
+                  Already have an account? Log in
+                </a>
+              </form>
+              {/* <p className="text-center text-sm text-gray-500 mt-4">
+                Copyright 2024-Schedule Nest
+              </p> */}
+            </div>
+          </div>
+        </div>
+  )
+}
