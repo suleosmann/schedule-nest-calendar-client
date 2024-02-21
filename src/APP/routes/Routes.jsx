b0f3e1007e175dc1b01a7c3d715a584a8540988b
mgkEdits <elijahnyasiando.mn@gmail.com>
2024-02-21 08:41:24 +0300

    routes

--- src/APP/routes/Routes.jsx
+++ src/APP/routes/Routes.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from './../App'
+
+import Authentication from './../modules/authentication/authentication'
+import Login from './../modules/authentication/Login'
+import Signup from './../modules/authentication/Signup'
+import UpdatePwd from './../modules/authentication/updatePwd'
+
+import Dashboard from './../modules/Home'
+import Calendar from './../modules/calendar'
+
+import Profile from './../modules/profile/VeiwProfile'
 
 export default function Routes() {
   return (
-    <div>Routes</div>
+    <Router>
+      <Routes>
+        <Route path='/Home' element={Home}>
+
+          <Route path='/authentication' element={}>
+          </Route>
+
+          <Route path='' element={}>
+          </Route>
+
+          <Route path='' element={}>
+          </Route>
+
+          <Route path='/dashboard' element={Dashboard}>
+          </Route>
+
+          <Route path='/profile' element={Profile}>
+          </Route>
+
+        </Route>
+      </Routes>
+    </Router>
   )
 }
