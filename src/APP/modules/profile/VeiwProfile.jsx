4192786c15d969f3df6dce93bc91afaad6cf7962
StormRobert <stormarasa2@gmail.com>
2024-02-29 14:50:10 +0300

    profile editeed

--- src/APP/modules/profile/VeiwProfile.jsx
+++ src/APP/modules/profile/VeiwProfile.jsx
@@ -42,34 +42,30 @@ export default function ViewProfile() {
     <>
       {userInfo && (
         <>
-          <section
-            className="flex h-full py-56 w-full bottom-0 right-0 bg-gray-200"
-            style={{
-              zIndex: 0,
-              background: "linear-gradient(to bottom, #f0f0f0, #5bc647)", // Replace these colors with your desired gradient
-            }}
-          >
-            <div className="container flex mx-24 mt-44 pt-12">
-              <div className="relative flex flex-col min-w-1 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-72 pt-12">
-                <div className="px-36">
-                  <div className="flex flex-wrap justify-center">
-                    {/* Profile Picture Section */}
-                    <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                      <div className="relative">
-                        <img
-                          src={SuleImage} // image URL
-                          alt="Profile picture"
-                          className="shadow-xl rounded-full h-auto align-botttom border-opacity-100 absolute lg:-ml-16 w-32 h-32"
-                          style={{ maxWidth: "150px" }}
-                        />
-                      </div>
-                    </div>
-
-                    {/*Button Section */}
-                    <div className="w-full lg:w-4/6 px-0 lg:order-3 lg:text-right lg:self-center">
-                      <div className="py-6 px-18 mt-3 sm:mt-0">
+          <div className="pl-0 md:pl-4 transition-all w-full">
+            <div className="p-4 h-64 w-full ">
+              <div className = "relative w-full h-4  scale-y-25 scale-x-95 ">
+              <img src={BackgroundImage} className="w-full h-64 object-cover rounded-lg " alt="bg picture"/>
+              </div>
+              <div style={{  background: "linear-gradient(to bottom, rgba(240, 240, 240, 0.5), rgba(91, 198, 71, 0.5))" }} 
+              className="relative rounded-lg shadow-xl absolute top-64 mx-2 ml-5 left-1/2 px-36 transform -translate-x-1/2">
+              <img
+                  src={SuleImage} 
+                  alt="Profile picture"
+                  className="py-2 shadow-xl rounded-full border-opacity-100 w-32 h-32"      
+                    />
+                  <div >
+                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800">
+                   {userInfo.name}
+                 </h3>
+                 <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
+                   <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
+                   {userInfo.profession}
+                 </div>
+                 <div className="w-full scale-x-100 lg:w-4/6 px-0 lg:order-3 lg:text-right lg:self-center">
+                      <div className="flex py-6 px-18 mt-3 sm:mt-0">
                         <button
-                          className="bg-green-500 active:bg-green-900 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none focus:ring focus:ring-green-900 sm:mr-2 mb-1"
+                          className=" bg-green-400 hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus:ring focus:ring-green-900 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded border border-gray-500 hover:border-transparent rounded focus:outline-none focus:shadow-outline sm:mr-2 mb-1"
                           type="button"
                           style={{ transition: "all .15s ease" }}
                           onClick={() => setShowEditProfileModal(true)}
@@ -77,7 +73,7 @@ export default function ViewProfile() {
                           Edit Profile
                         </button>
                         <button
-                          className="bg-red-500 active:bg-orange-900 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none focus:ring focus:ring-green-900 sm:mr-2 mb-1"
+                          className="bg-orange-500 hover:bg-red-400 active:bg-red-600 focus:outline-none focus:ring focus:ring-green-900 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded border border-gray-500 hover:border-transparent rounded focus:outline-none focus:shadow-outline sm:mr-2 mb-1"
                           type="button"
                           style={{ transition: "all .15s ease" }}
                           onClick={() => setShowDeleteModal(true)}
@@ -85,20 +81,8 @@ export default function ViewProfile() {
                           Delete Account
                         </button>
                       </div>
-                    </div>
-
-                    {/* Card Section */}
-                    <div className="w-full lg:w-9/12 px-4 lg:order-1 ">
-                      <div className="text-center mt-12">
-                        <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
-                          {userInfo.name}
-                        </h3>
-                        <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
-                          <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
-                          {userInfo.profession}
-                        </div>
-                        <div className="mt-10 py-4 bg-white border rounded-lg shadow-md ">
-                          <div className="text-center">
+                      <div className="mt-10 py-4 bg-white border rounded-lg shadow-md ">
+                          <div className="text-left ml-3">
                             <h4 className="mb-4 leading-relaxed text-grey-900 font-bold text-3xl">
                               Contact Details
                             </h4>
@@ -114,27 +98,17 @@ export default function ViewProfile() {
                             </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="mt-10 py-10 border-t border-gray-300 text-center">
-                        <div className="mt-6 py-6 bg-white border rounded-lg shadow-md">
-                          <div className="text-center">
-                            <div className="flex flex-wrap justify-center">
-                              <div className="w-full lg:w-9/12 px-4">
+                        <div className="mt-10 py-10 border-t border-gray-500 text-center">
+                        <div className="mt-6 py-6 bg-green-300 border rounded-lg shadow-md">
+                          <div className="">
+                            <div className="flex flex-wrap ">
+                              <div className="w-full text-left ml-3 lg:w-9/12 px-4">
                                 <h4 className="mb-4 leading-relaxed text-grey-900 font-bold text-3xl">
                                   About
                                 </h4>
-
                                 <p className="mb-4 text-lg leading-relaxed text-gray-800">
                                   {userInfo.about}
                                 </p>
-
-                                <a
-                                  href="#pablo"
-                                  className="font-normal text-pink-500"
-                                  onClick={(e) => e.preventDefault()}
-                                >
-                                  Show more
-                                </a>
                               </div>
                             </div>
                           </div>
@@ -142,10 +116,10 @@ export default function ViewProfile() {
                       </div>
                     </div>
                   </div>
-                </div>
+                 
               </div>
             </div>
-          </section>
+          </div>
           {showEditProfileModal && (
             <EditProfile
               userInfo={userInfo}
